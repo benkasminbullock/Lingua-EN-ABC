dfcdcaf8935bbc72414665f371b85bdd05304739
Ben Bullock <benkasminbullock@gmail.com>
2013-03-14 16:40:24 +0900

    Initial commit of Lingua-EN-ABC

---
+++ lib/Lingua/EN/ABC.pm
@@ -0,0 +1,27 @@
+=encoding UTF-8
+
+=head1 NAME
+
+Lingua::EN::ABC - abstract here.
+
+=head1 SYNOPSIS
+
+    use Lingua::EN::ABC;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
+package Lingua::EN::ABC;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+use Carp;
+our $VERSION = 0.01;
+1;
---
+++ t/Lingua-EN-ABC.t
@@ -0,0 +1,10 @@
+# This is a test for module Lingua::EN::ABC.
+
+use warnings;
+use strict;
+use Test::More;
+use Lingua::EN::ABC;
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
