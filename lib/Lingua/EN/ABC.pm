6dad5ccadbe023da39c0b35b3b21e368e2839749
Ben Bullock <benkasminbullock@gmail.com>
2014-01-25 11:38:59 +0900

    Documentation etc. updates.

--- lib/Lingua/EN/ABC.pm
+++ lib/Lingua/EN/ABC.pm
@@ -11,6 +11,10 @@ Lingua::EN::ABC - American, British, and Canadian English
 
 =head1 DESCRIPTION
 
+This is an experimental module. This module offers functions to
+convert between the spellings and vocabulary of American, British, and
+Canadian versions of English. The interface is described below.
+
 =head1 FUNCTIONS
 
 =head2 a2b
@@ -18,19 +22,69 @@ Lingua::EN::ABC - American, British, and Canadian English
     my $british = a2b ('color');
     # $british = 'colour'.
 
-Convert British spellings into American spellings.
+An option C<oxford> controls whether to use Oxford spelling (realize
+rather than realise):
+
+    my $oxford_british = a2b ('realize', oxford => 1);
 
 =head2 b2a
 
+    my $american = b2a ('the colour of my pyjamas');
+    # $american = 'the color of my pajamas'
+
+Convert British spellings into American spellings.
+
 =head2 a2c
+
+    my $canadian = a2c ('the color');
+    # $canadian = 'the colour'
+
+Convert American to Canadian spelling.
 
 =head2 c2a
 
+    my $american = c2a ('the color');
+    # $american = 'the colour'
+
+Convert Canadian to American spelling.
+
 =head2 b2c
+
+    my $canadian = b2c ('the programme');
+    # $canadian = 'the program'
+
+Convert British to Canadian spelling.
 
 =head2 c2b
 
+    my $british = c2b ($canadian);
+
+Convert Canadian to British spelling. An option C<oxford> controls
+whether to use Oxford spelling (realize rather than realise):
+
+    my $oxford_british = c2b ($canadian, oxford => 1);
+
+=head1 SEE ALSO
+
+This module's design is up for discussion. You can either use the
+Github issues page at
+L<https://github.com/benkasminbullock/Lingua-EN-ABC/issues> or the
+Prepan site at L<http://prepan.org/module/nXWEF3to2nu>.
+
+The file containing the spelling variations is "abc.txt" in the top
+directory of the distribution.
+
+=head1 AUTHOR
+
+Ben Bullock, <bkb@cpan.org>
+
+=head1 LICENCE
+
+This program and associated files may be used, copied, distributed,
+and modified under the same terms as Perl itself.
+
 =cut
+
 package Lingua::EN::ABC;
 require Exporter;
 @ISA = qw(Exporter);
@@ -43,7 +97,7 @@ use strict;
 use Carp;
 use JSON::Parse 'json_file_to_perl';
 
-our $VERSION = 0.01;
+our $VERSION = 0.03;
 
 # Load the data from the file.
 
@@ -75,12 +129,31 @@ sub a2b
 sub b2a
 {
     my ($text) = @_;
+    for my $e (@$abc) {
+	my $american = $e->{a};
+	my $british = $e->{b};
+	if ($e->{aam}) {
+	    $american .= "/$e->{b}";
+	}
+	$text =~ s/\b$british\b/$american/g;
+    }
     return $text;
 }
 
 sub a2c
 {
     my ($text) = @_;
+    for my $e (@$abc) {
+	if ($e->{ca} || $e->{o}) {
+	    next;
+	}
+	my $american = $e->{a};
+	my $canadian = $e->{b};
+	if ($e->{bam}) {
+	    $canadian .= "/$e->{a}";
+	}
+	$text =~ s/\b$american\b/$canadian/g;
+    }
     return $text;
 }
 
@@ -100,21 +173,41 @@ sub c2a
 	    # programme/program.
 	    $canadian .= "/$e->{a}";
 	}
-	$text =~ s/\b$american\b/$canadian/g;
+	$text =~ s/\b$canadian\b/$american/g;
     }
     return $text;
 }
 
 sub c2b
 {
-my ($text) = @_;
-return $text;
+    my ($text, %options) = @_;
+    for my $e (@$abc) {
+	if ($options{oxford} && $e->{oxford}) {
+	    # Do not convert Oxford spellings.
+	    next;
+	}
+	# Here we do not check the value of ca, but just convert any
+	# American spellings which may be found in the Canadian text.
+	my $canadian = $e->{a};
+	my $british = $e->{b};
+	$text =~ s/\b$canadian\b/$british/g;
+    }
+    return $text;
 }
 
 sub b2c
 {
-my ($text) = @_;
-return $text;
+    my ($text) = @_;
+    for my $e (@$abc) {
+	if ($e->{ca}) {
+	    # Convert the word if this is spelt differently in Canada
+	    # and the UK.
+	    my $canadian = $e->{a};
+	    my $british = $e->{b};
+	    $text =~ s/\b$british\b/$canadian/g;
+	}
+    }
+    return $text;
 }
 
 1;
--- t/Lingua-EN-ABC.t
+++ t/Lingua-EN-ABC.t
@@ -5,13 +5,33 @@ use strict;
 use Test::More;
 use Lingua::EN::ABC ':all';
 
-my $american = 'I realize the color and flavor in the center of my pajamas';
-my $british = a2b ($american);
+my $american;
+my $british;
+
+$american = 'I realize the color and flavor in the center of my pajamas';
+$british = a2b ($american);
 is ($british, 'I realise the colour and flavour in the centre of my pyjamas');
 $american = 'I realize you like this flavor';
 $british = a2b ($american, oxford => 1);
 is ($british, 'I realize you like this flavour'); 
+
+my $canadian = 'the centre of the program is ten metres';
+$american = c2a ($canadian);
+like ($american, qr/center/);
+$british = c2b ($canadian);
+like ($british, qr/programme/);
+
+my $canadian2 = a2c ($american);
+
+is ($canadian2, $canadian);
+
+$canadian2 = b2c ($british);
+
+is ($canadian2, $canadian);
+
 done_testing ();
+
+
 # Local variables:
 # mode: perl
 # End:
